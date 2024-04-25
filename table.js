040be998d21e6ecc0fdd688bb26b3364e8343d5a
Bijay <Bijay Rauniyar>
2024-04-25 11:43:57 +0545

    files added

---
+++ table.js
@@ -0,0 +1,11 @@
+let actors ={
+    "kapoor": {
+        "name": "Shahid Kapoor",
+        "movies": ["Kabir Singh", "Haider"]
+    },
+    "khan": {
+        "name": "Aamir Khan",
+        "movies": ["Dangal", "PK"]
+    },
+}
+console.table(actors)
